66df9436d249e1fe476290254cfacded07ee705a
Richa <richa.cs21@bmsce.ac.in>
2023-09-05 01:00:32 +0530

    Updated

--- FoodOrderMain/config/constants.php
+++ FoodOrderMain/config/constants.php
@@ -11,7 +11,7 @@
     define('DB_PASSWORD', '');
     define('DB_NAME', 'foodorder');
     
-    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
+    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME) or die(mysqli_error()); //Database Connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
 
 
