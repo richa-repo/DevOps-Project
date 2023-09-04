a916850d7520bfa52a9b0ae81a5e8083b6cddfae
Richa <richa.cs21@bmsce.ac.in>
2023-09-04 20:32:04 +0530

    altered

--- FoodOrderMain/config/constants.php
+++ FoodOrderMain/config/constants.php
@@ -1,13 +1,12 @@
 <?php 
-    //Start Session
-    session_start();
+    //session_start();
 
 
     //Create Constants to Store Non Repeating Values
     define('SITEURL', 'http://localhost/food-order/');
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'Spikey!89');
+    define('DB_PASSWORD', 'root');
     define('DB_NAME', 'foodorder');
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
