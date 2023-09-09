5ef3599c2c29939022340f3a996d6cae783f5877
Richa <richa.cs21@bmsce.ac.in>
2023-09-09 22:03:13 +0530

    Updated

--- php/registration.php
+++ php/registration.php
@@ -11,21 +11,21 @@
 </head>
 <body>
 <?php
-    require('db.php');
+    require('constants.php');
     // When form submitted, insert values into the database.
     if (isset($_REQUEST['username'])) {
         // removes backslashes
         $username = stripslashes($_REQUEST['username']);
         //escapes special characters in a string
-        $username = mysqli_real_escape_string($con, $username);
+        $username = mysqli_real_escape_string($conn, $username);
         $email    = stripslashes($_REQUEST['email']);
-        $email    = mysqli_real_escape_string($con, $email);
+        $email    = mysqli_real_escape_string($conn, $email);
         $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($con, $password);
+        $password = mysqli_real_escape_string($conn, $password);
         $create_datetime = date("Y-m-d H:i:s");
         $query    = "INSERT into `users` (username, password, email, create_datetime)
                      VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
-        $result   = mysqli_query($con, $query);
+        $result   = mysqli_query($conn, $query);
         if ($result) {
             echo "<div class='form'>
                   <h3>You are registered successfully.</h3><br/>
@@ -39,6 +39,8 @@
         }
     } else {
 ?>
+
+
     <form class="form" action="" method="post">
         <h1 class="login-title">Registration</h1>
         <input type="text" class="login-input" name="username" placeholder="Username" required /><br>
