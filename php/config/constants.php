76e3f428dc8993eb90c9f066415485ac19d7b72c
Richa <richa.cs21@bmsce.ac.in>
2023-09-09 20:33:07 +0530

    Updated

--- php/config/constants.php
+++ php/config/constants.php
@@ -5,7 +5,7 @@
 
 
     //Create Constants to Store Non Repeating Values
-    define('SITEURL', 'http://localhost/food-order/');
+    define('SITEURL', 'http://localhost:8080/');
     define('LOCALHOST', 'mysql_db');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', 'root');
