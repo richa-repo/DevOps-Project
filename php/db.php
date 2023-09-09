3caff01d4c3e900cecc790ddfe3759917b15a19e
Richa <richa.cs21@bmsce.ac.in>
2023-09-09 21:34:56 +0530

    Updated

--- php/db.php
+++ php/db.php
@@ -2,6 +2,7 @@
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
     $con = mysqli_connect("mysql_db","root","","foodorder");
+    //
     //$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME) or die(mysqli_error());
     // Check connection
     if (mysqli_connect_errno()){
